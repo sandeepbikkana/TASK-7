2201f88276c19114be0021506e638e35e60cb155
sandeepbikkana <sandeepbikkana@gmail.com>
2025-12-15 18:19:42 +0530

    Delete s3.tf

--- s3.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terra-tf-buck-12"
-    key            = "strapi/ecs/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-    # dynamodb_table = "terraform-locks"
-  }
-}
