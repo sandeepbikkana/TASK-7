5869636609c1a398f43e708c34042c9c9ab0de1f
sandeepbikkana <sandeepbikkana@gmail.com>
2025-12-17 15:24:09 +0530

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terra-tf-buck-12"
+    bucket         = "sandeep-tfstate-buck"
     key            = "strapi/ecs/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
