c97d69323f40b649e0da2562f8bf96df9220c4af
sandeepbikkana <sandeepbikkana@gmail.com>
2025-12-17 15:39:18 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,14 @@
 output "alb_url" {
-  value = "http://${aws_lb.strapi.dns_name}"
+  description = "Public URL of Strapi Application Load Balancer"
+  value       = "http://${aws_lb.sandeep_strapi.dns_name}"
 }
 
 output "rds_endpoint" {
-  value = aws_db_instance.strapi.address
+  description = "RDS PostgreSQL endpoint"
+  value       = aws_db_instance.sandeep_strapi.address
 }
-output "log_group" {
-  value = aws_cloudwatch_log_group.strapi.name
+
+output "log_group_name" {
+  description = "CloudWatch Log Group for ECS tasks"
+  value       = aws_cloudwatch_log_group.sandeep_strapi.name
 }
