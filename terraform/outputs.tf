bc5a11ccbc6533451e4086a10b4e014b454934f7
sandeepbikkana <sandeepbikkana@gmail.com>
2025-12-22 20:47:59 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "alb_url" {
   description = "Public URL of the Strapi Application Load Balancer"
-  value       = "http://${aws_lb.strapi.dns_name}"
+  value       = "http://${aws_lb.sandeep_strapi.dns_name}"
 }
 
 output "ecs_cluster_name" {
@@ -10,7 +10,7 @@ output "ecs_cluster_name" {
 
 output "ecs_service_name" {
   description = "ECS Service name"
-  value       = aws_ecs_service.strapi.name
+  value       = aws_ecs_service.sandeep_strapi.name
 }
 
 output "rds_endpoint" {
@@ -19,16 +19,21 @@ output "rds_endpoint" {
 }
 
 output "cloudwatch_log_group" {
-  description = "CloudWatch Log Group for ECS"
+  description = "CloudWatch Log Group for ECS Strapi logs"
   value       = aws_cloudwatch_log_group.sandeep_strapi.name
 }
 
-output "codedeploy_application" {
+output "codedeploy_application_name" {
   description = "CodeDeploy application name"
-  value       = aws_codedeploy_app.ecs.name
+  value       = aws_codedeploy_app.sandeep.name
 }
 
-output "codedeploy_deployment_group" {
+output "codedeploy_deployment_group_name" {
   description = "CodeDeploy deployment group name"
-  value       = aws_codedeploy_deployment_group.ecs.deployment_group_name
+  value       = aws_codedeploy_deployment_group.sandeep.deployment_group_name
+}
+
+output "cloudwatch_dashboard_name" {
+  description = "CloudWatch dashboard name for Strapi monitoring"
+  value       = aws_cloudwatch_dashboard.sandeep.dashboard_name
 }
