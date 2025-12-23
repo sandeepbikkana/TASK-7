71d41fcf826beda1d90692c57aef44942ef54c85
sandeepbikkana <sandeepbikkana@gmail.com>
2025-12-23 06:12:51 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -268,15 +268,10 @@ resource "aws_ecs_service" "sandeep_strapi" {
 # CODEDEPLOY
 ################################
 
-resource "aws_codedeploy_app" "sandeep" {
-  name             = "sandeep-strapi-codedeploy"
-  compute_platform = "ECS"
-}
-
 resource "aws_codedeploy_deployment_group" "sandeep" {
   app_name              = aws_codedeploy_app.sandeep.name
   deployment_group_name = "sandeep-strapi-dg"
-  service_role_arn      = aws_iam_role.sandeep_codedeploy.arn
+  service_role_arn      = data.aws_iam_role.codedeploy.arn
 
   deployment_style {
     deployment_type   = "BLUE_GREEN"
@@ -285,24 +280,40 @@ resource "aws_codedeploy_deployment_group" "sandeep" {
 
   deployment_config_name = "CodeDeployDefault.ECSCanary10Percent5Minutes"
 
+  blue_green_deployment_config {
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+    }
+
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+      termination_wait_time_in_minutes = 5
+    }
+  }
+
   auto_rollback_configuration {
     enabled = true
     events  = ["DEPLOYMENT_FAILURE"]
   }
 
   ecs_service {
-    cluster_name = aws_ecs_cluster.sandeep_strapi.name
-    service_name = aws_ecs_service.sandeep_strapi.name
+    cluster_name = aws_ecs_cluster.sandeep.name
+    service_name = aws_ecs_service.sandeep.name
   }
 
   load_balancer_info {
     target_group_pair_info {
       prod_traffic_route {
-        listener_arns = [aws_lb_listener.sandeep_http.arn]
+        listener_arns = [aws_lb_listener.http.arn]
       }
 
-      target_group { name = aws_lb_target_group.sandeep_blue.name }
-      target_group { name = aws_lb_target_group.sandeep_green.name }
+      target_group {
+        name = aws_lb_target_group.blue.name
+      }
+
+      target_group {
+        name = aws_lb_target_group.green.name
+      }
     }
   }
 }
